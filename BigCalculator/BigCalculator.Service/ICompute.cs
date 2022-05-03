63ac657640ccf609e942e4b7a02a7362ec781aa6
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2022-05-03 10:49:17 +0300

    Mul operation

--- BigCalculator/BigCalculator.Service/ICompute.cs
+++ BigCalculator/BigCalculator.Service/ICompute.cs
@@ -5,7 +5,7 @@
         Dictionary<string, string> ComputeCalculus(string expression, Dictionary<string, string> terms);
 
         string Sum(string a, string b);
-        string Mul(string a, string b);
+        string Mul(int[] a, int[] b);
         string Diff(string a, string b);
         string Div(int[] a, int[] b);
         string Pow(string a, string b);
