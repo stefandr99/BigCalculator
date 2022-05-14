5187d0f17efc65e54f4ba232453c1aaa1c12a389
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2022-05-14 10:26:35 +0300

    unit tests for Mul and Diff

--- BigCalculator/BigCalculator.UnitTests/Calculus/CalculatorUnitTests.cs
+++ BigCalculator/BigCalculator.UnitTests/Calculus/CalculatorUnitTests.cs
@@ -64,5 +64,83 @@ namespace BigCalculator.UnitTests.Calculus
             //Assert
             Assert.Equal("0", result);
         }
+
+        [Fact]
+        public void Given_Mul_When_OperandIsZero_Then_ReturnsZero()
+        {
+            //Arrange
+            var calculator = CreateNewCalculator();
+            int[] firstOperand = new int[] { 1, 1 };
+            int[] secondOperand = new int[] { 0 };
+
+            //Act
+            var result = calculator.Mul(firstOperand, secondOperand);
+
+            //Assert
+            Assert.Equal("0", result);
+        }
+
+        [Fact]
+        public void Given_Mul_When_OperandIsOne_Then_ReturnsValueOfTheOtherOperand()
+        {
+            //Arrange
+            var calculator = CreateNewCalculator();
+            int[] firstOperand = new int[] { 1, 0 };
+            int[] secondOperand = new int[] { 1 };
+            string expected = "10";
+
+            //Act
+            var result = calculator.Mul(firstOperand, secondOperand);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_Diff_When_SecondOperandIsZero_Then_ResultIsEqualToFirstOpenrand()
+        {
+            //Arrange
+            var calculator = CreateNewCalculator();
+            int[] firstOperand = new int[] { 1, 0 };
+            int[] secondOperand = new int[] { 0 };
+            string expected = "10";
+
+            //Act
+            var result = calculator.Diff(firstOperand, secondOperand);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_Diff_When_NumbersAreEqual_Then_ResultIsZero()
+        {
+            //Arrange
+            var calculator = CreateNewCalculator();
+            int[] operand = new int[] { 1, 2, 3 };
+            string expected = "0";
+
+            //Act
+            var result = calculator.Diff(operand, operand);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Given_Diff_When_FirstOperandIsSmallerThanSecondOperand_Then_ReturnsStatusCodeMinusOne()
+        {
+            //Arrange
+            var calculator = CreateNewCalculator();
+            int[] firstOperand = new int[] { 1, 2, 3 };
+            int[] secondOperand = new int[] { 1, 2, 4 };
+            string expected = "-1";
+
+            //Act
+            var result = calculator.Diff(firstOperand, secondOperand);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
